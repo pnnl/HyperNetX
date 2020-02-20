a6cabdd95b3d1cda7c189d24e0c1f51b2e6af516
Brenda Praggastis <brenda.praggastis@pnnl.gov>
2020-02-20 11:00:28 -0500

    updating documentation and version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup 
 import sys, os.path
 
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 
 if sys.version_info < (3,6):
     sys.exit('HyperNetX requires Python 3.6 or later.')
