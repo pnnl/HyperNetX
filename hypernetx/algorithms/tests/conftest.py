5ea6c61a6028fdd9b44f013891530b7fde96c889
Brenda Praggastis <brenda.praggastis@pnnl.gov>
2020-09-06 16:34:21 -0700

    added fixtures for s_centrality tests

--- hypernetx/algorithms/tests/conftest.py
+++ hypernetx/algorithms/tests/conftest.py
@@ -14,6 +14,21 @@ class TriLoop():
         self.hypergraph = hnx.Hypergraph(self.edgedict, name='TriLoop')
 
 
+class Fish():
+    """Example hypergraph with 2 two 1-cells and 1 2-cell forming a loop"""
+
+    def __init__(self):
+        A, B, C, D, E, F, G, H = 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'
+        AB, BC, ACD, BEH, CF, AG = 'AB', 'BC', 'ACD', 'BEH', 'CF', 'AG'
+        self.edgedict = {AB: {A, B}, BC: {B, C}, ACD: {A, C, D}, BEH: {B, E, H}, CF: {C, F}, AG: {A, G}}
+        self.hypergraph = hnx.Hypergraph(self.edgedict, name='Fish')
+
+
 @pytest.fixture
 def triloop():
     return TriLoop()
+
+
+@pytest.fixture
+def fish():
+    return Fish()
