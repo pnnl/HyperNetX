1ac25e6c190eebd7b5bfaf9b03cc5508e0cc60fa
Brenda Praggastis <brenda.praggastis@pnnl.gov>
2021-06-09 13:10:40 -0700

    updating imports for colab

--- hypernetx/utils/__init__.py
+++ hypernetx/utils/__init__.py
@@ -1,3 +1,3 @@
 from .extras import HNXCount, DefaultOrderedDict, remove_row_duplicates, create_labels
 from .decorators import not_implemented_for
-from .toys import *
+from .toys import HarryPotter, LesMis, lesmis_hypergraph_from_df, book_tour, TransmissionProblem
