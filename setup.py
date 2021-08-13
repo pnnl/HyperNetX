eb6e812db750216fa7fb97577b014148b72decfe
Brenda Praggastis <brenda.praggastis@pnnl.gov>
2021-08-13 12:53:28 -0700

    updated long description to pass for PyPI

--- setup.py
+++ setup.py
@@ -33,6 +33,41 @@ setup(
         "celluloid>=0.2.0",
     ],
     license="3-Clause BSD license",
+    long_description="""
+    The HyperNetX library provides classes and methods for the analysis
+    and visualization of complex network data modeled as hypergraphs. 
+    The library generalizes traditional graph metrics.
+
+    HypernetX was developed by the Pacific Northwest National Laboratory for the
+    Hypernets project as part of its High Performance Data Analytics (HPDA) program.
+    PNNL is operated by Battelle Memorial Institute under Contract DE-ACO5-76RL01830.
+
+    * Principle Developer and Designer: Brenda Praggastis
+    * Visualization: Dustin Arendt, Ji Young Yun
+    * High Performance Computing: Tony Liu, Andrew Lumsdaine
+    * Principal Investigator: Cliff Joslyn
+    * Program Manager: Mark Raugas, Brian Kritzstein
+    * Contributors: Sinan Aksoy, Dustin Arendt, Cliff Joslyn, Nicholas Landry, Andrew Lumsdaine, Tony Liu, Brenda Praggastis, Emilie Purvine, Mirah Shi, Francois Theberge
+
+    The code in this repository is intended to support researchers modeling data
+    as hypergraphs. We have a growing community of users and contributors.
+    Documentation is available at: <https://pnnl.github.io/HyperNetX/>
+
+    For questions and comments contact the developers directly at: <hypernetx@pnnl.gov>
+
+    **New Features of Version 1.0:**
+
+    1. Hypergraph construction can be sped up by reading in all of the data at once. In particular the hypergraph constructor may read a Pandas dataframe object and create edges and nodes based on column headers. The new hypergraphs are given an attribute `static=True`.
+    2. A C++ addon called [NWHy](docs/build/nwhy.html) can be used in Linux environments to support optimized hypergraph methods such as s-centrality measures.
+    3. A JavaScript addon called [Hypernetx-Widget](docs/build/widget.html) can be used to interactively inspect hypergraphs in a Jupyter Notebook.
+    4. Four new tutorials highlighting the s-centrality metrics, static Hypergraphs, [NWHy](docs/build/nwhy.html), and [Hypernetx-Widget](docs/build/widget.html).
+
+    **New Features of Version 1.1**
+
+    1. Static Hypergraph refactored to improve performance across all methods.
+    2. Added modules and tutorials for Contagion Modeling, Community Detection, Clustering, and Hypergraph Generation.
+    3. Cell weights for incidence matrices may be added to static hypergraphs on construction.
+    """,
     extras_require={
         "testing": ["pytest>=4.0"],
         "tutorials": ["jupyter>=1.0"],
