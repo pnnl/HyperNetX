937e7f72bf82bae9a0ef19b775547debaca608dd
Madelyn Shapiro <madelyn.shapiro@pnnl.gov>
2022-02-02 15:17:12 -0800

    added aggregation of cell weights for duplicate rows

--- hypernetx/classes/dfentity.py
+++ hypernetx/classes/dfentity.py
@@ -9,6 +9,7 @@ class StaticEntity(object):
         self,
         data,  # DataFrame, Dict of Lists, List of Lists, or np array
         weights=None,  # array-like of values corresponding to rows of data
+        aggregateby="sum",
     ):
         if isinstance(data, pd.DataFrame):
             # dataframe case
@@ -30,20 +31,24 @@ class StaticEntity(object):
 
         if isinstance(weights, (list, np.ndarray)) and len(weights) == len(self._data):
             self._data["cell_weights"] = weights
-            self._cell_weights = self._data.cell_weights
             self._cell_weight_cols = ["cell_weights"]
         elif weights in self._data:
-            self._cell_weights = self._data[weights]
             self._cell_weight_cols = [weights]
         else:
-            self._cell_weights = None
-            self._cell_weight_cols = []
+            self._data["cell_weights"] = np.ones(len(self._data), dtype=int)
+            self._cell_weight_cols = ["cell_weights"]
 
         self._data_cols = list(self._data.columns.drop(self._cell_weight_cols))
+
+        if aggregateby is None:
+            self._data.drop_duplicates(subset=self._data_cols, inplace=True)
+        else:
+            self._data = self._data.groupby(
+                self._data_cols, as_index=False, sort=False
+            ).agg({w: aggregateby for w in self._cell_weight_cols})
+
         self._dimensions = tuple(self._data[self._data_cols].nunique())
         self._dimsize = len(self._data_cols)
-
-        # TODO: aggregate duplicate data rows
 
     @property
     def data(self):
