4c504d02cf512bff184448c0848f215d7de1c7ae
Noah Farris <noah.farris@pnnl.gov>
2022-05-06 11:35:12 -0700

    Static entity docstrings and some pep8 compliance

--- hypernetx/classes/staticentityset.py
+++ hypernetx/classes/staticentityset.py
@@ -59,23 +59,55 @@ class StaticEntitySet(StaticEntity):
             properties=properties
         )
 
-        self._cell_properties = self._create_cell_properties(cell_properties) if self._dimsize == 2 else None
+        self._cell_properties = self._create_cell_properties(cell_properties) /
+        if self._dimsize == 2 else None
 
     @property
     def cell_properties(self):
+        # Dev Note: 
         return self._cell_properties
 
     @property
     def memberships(self):
+        """
+        Reverses the elements dictionary
+
+        Returns
+        -------
+        dict
+            Same as elements_by_level with level1 = 1, level2 = 0.
+        """
         if self._dimsize == 1:
             return self._state_dict.get("memberships")
 
         return super().memberships
 
     def restrict_to_levels(
-        self, levels, weights=False, aggregateby="sum", keep_memberships=True, **kwargs
-    ):
-        restricted = super().restrict_to_levels(levels, weights, aggregateby, **kwargs)
+                           self, levels, weights=False, aggregateby="sum",
+                           keep_memberships=True, **kwargs):
+        """
+        Limit Static Entity data to specific levels
+
+        Parameters
+        ----------
+        levels : array
+            index of labels in data
+        weights : bool, optional, default : False
+            Whether or not to aggregate existing weights in self when
+            restricting to levels. If False then weights will be assigned 1.
+        aggregateby : str, optional, {None, 'last', count', 'sum', 'mean',
+            'median', max', 'min', 'first', 'last'}, default : 'count' Method
+            to aggregate cell_weights of duplicate rows in setsystem of type
+            pandas.DataFrame. If None then all cell_weights will be set to 1.
+        uid : None, optional
+
+        Returns
+        -------
+        Static Entity class
+            hnx.classes.staticentity.StaticEntity
+        """
+        restricted = super().restrict_to_levels(levels, weights, aggregateby,
+                                                **kwargs)
 
         if keep_memberships:
             restricted._state_dict["memberships"] = self.memberships
@@ -83,33 +115,71 @@ class StaticEntitySet(StaticEntity):
         return restricted
 
     def restrict_to(self, indices, **kwargs):
+        """
+        Limit Static Entityset data to specific indices of keys
+
+        Parameters
+        ----------
+        indices : array
+            array of indices in keys
+        uid : None, optional
+
+        Returns
+        -------
+        StaticEntitySet
+            hnx.classes.staticentity.StaticEntitySet
+
+        """
         return self.restrict_to_indices(indices, **kwargs)
 
     def _create_cell_properties(self, props):
-        index = pd.MultiIndex(levels=([],[]),codes=([],[]),names=self._data_cols)
-        kwargs = {'index':index,'name':'cell_properties'}
+        # Dev Note:
+        index = pd.MultiIndex(levels=([], []), codes=([], []),
+                              names=self._data_cols)
+        kwargs = {'index': index, 'name': 'cell_properties'}
         if props:
-            cells = [(edge,node) for edge in props for node in props[edge]]
+            cells = [(edge, node) for edge in props for node in props[edge]]
             index = pd.MultiIndex.from_tuples(cells, names=self._data_cols)
             data = [props[edge][node] for edge, node in index]
-            kwargs.update(index=index,data=data)
+            kwargs.update(index=index, data=data)
         return pd.Series(**kwargs)
 
     def assign_cell_properties(self, props):
+        # Dev Note: Not sure on this one
         if self._dimsize == 2:
             cell_properties = self._create_cell_properties(props)
 
             if not self._cell_properties.empty:
-                cell_properties = update_properties(self._cell_properties, cell_properties)
+                cell_properties = update_properties(self._cell_properties,
+                                                    cell_properties)
 
             self._cell_properties = cell_properties
 
-    def collapse_identical_elements(self, return_equivalence_classes=False,**kwargs):
-        collapse = self._dataframe[self._data_cols].groupby(self._data_cols[0],as_index=False).agg(frozenset)
-        agg_kwargs = {'name': (self._data_cols[0],lambda x:  f'{x.iloc[0]}:{len(x)}')}
+    def collapse_identical_elements(self, return_equivalence_classes=False,
+                                    **kwargs):
+        """
+        Returns StaticEntitySet after collapsing elements if they have same
+        children If no elements share same children, a copy of the original
+        StaticEntitySet is returned
+
+        Parameters
+        ----------
+        uid : None, optional
+        return_equivalence_classes : bool, optional
+            If True, return a dictionary of equivalence classes keyed by new
+            edge names
+
+
+        Returns
+        -------
+        StaticEntitySet
+            hnx.classes.staticentity.StaticEntitySet
+        """
+        collapse = self._dataframe[self._data_cols].groupby(self._data_cols[0], as_index=False).agg(frozenset)
+        agg_kwargs = {'name': (self._data_cols[0], lambda x:  f'{x.iloc[0]}: {len(x)}')}
         if return_equivalence_classes:
-            agg_kwargs.update(equivalence_class=(0,list))
-        collapse = collapse.groupby(self._data_cols[1],as_index=False).agg(**agg_kwargs)
+            agg_kwargs.update(equivalence_class=(0, list))
+        collapse = collapse.groupby(self._data_cols[1], as_index=False).agg(**agg_kwargs)
         collapse = collapse.set_index('name')
         new_entity_dict = collapse[self._data_cols[1]].to_dict()
         new_entity = StaticEntitySet(new_entity_dict, **kwargs)
@@ -117,4 +187,3 @@ class StaticEntitySet(StaticEntity):
             equivalence_classes = collapse.equivalence_class.to_dict()
             return new_entity, equivalence_classes
         return new_entity
-
