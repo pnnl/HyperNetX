1503f19c6d7aaf38f044dcff13df078bab9e4352
shap825 <madelyn.shapiro@pnnl.gov>
2022-08-11 12:42:16 -0700

    Defined dunder field "all"

--- hypernetx/classes/__init__.py
+++ hypernetx/classes/__init__.py
@@ -1,3 +1,5 @@
 from hypernetx.classes.entity import Entity
 from hypernetx.classes.entityset import EntitySet
 from hypernetx.classes.hypergraph import Hypergraph
+
+__all__ = ["Entity", "EntitySet", "Hypergraph"]
