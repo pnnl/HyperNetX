0a075f5d234f6aeae32099689ae209f145596bc9
Brenda Praggastis <39808911+brendapraggastis@users.noreply.github.com>
2023-05-02 13:07:29 -0700

    fixed toplexes bug

--- hypernetx/classes/hypergraph.py
+++ hypernetx/classes/hypergraph.py
@@ -1685,7 +1685,7 @@ class Hypergraph:
                 
 
 
-    def toplexes(self, name=None, collapse=False, use_reps=False, return_counts=True):
+    def toplexes(self, name=None):
         """
         Returns a :term:`simple hypergraph` corresponding to self.
 
@@ -1698,49 +1698,14 @@ class Hypergraph:
         Parameters
         ----------
         name: str, optional, default = None
-
-        # collapse: boolean, optional, default = False
-        #     Should the hypergraph be collapsed? This would preserve a link
-        #     between duplicate maximal sets. If False then only one of these
-        #     sets will be used and uniqueness will be up to sets of equal
-        #     size.
-
-        # use_reps: boolean, optional, default = False
-        #     If collapse=True then each toplex will be named by a
-        #     representative of the set of equivalent edges, default is False
-        #     (see collapse_edges).
-
-        return_counts: boolean, optional, default = True
-            # If collapse=True then each toplex will be named by a tuple of
-            # the representative of the set of equivalent edges and their count
-
         """
-        # TODO: There is a better way to do this....need to refactor
-        # if collapse:
-        #     if len(self.edges) > 20:  # TODO: Determine how big is too big.
-        #         msg = """
-        #         Collapsing a hypergraph can take a long time. It may be preferable to
-        #         collapse the graph first and pickle it then apply the toplex method
-        #         separately.
-        #         """
-        #         warnings.warn(msg)
-        #     temp = self.collapse_edges()
-        # else:
-        #     temp = self
-
-        if collapse:
-            msg = """
-            collapse, return_counts, and use_reps are no longer supported
-            keyword arguments and will throw an error in the next release.
-            """
-            warnings.warn(msg, DeprecationWarning)
 
         thdict = {}
-        for e in temp.edges:
-            thdict[e] = temp.edges[e]
+        for e in self.edges:
+            thdict[e] = self.edges[e]
 
         tops = []
-        for e in temp.edges:
+        for e in self.edges:
             flag = True
             old_tops = list(tops)
             for top in old_tops:
