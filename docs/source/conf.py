020e57bf12be36855d7046e3a878f8b609fece5c
Mark Bonicillo <mark.bonicillo@pnnl.gov>
2023-06-15 11:47:13 -0700

    bump: version 2.0.0.post1 → 2.0.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ import sys
 import os
 
 
-__version__ = "2.0.0.post1"
+__version__ = "2.0.1"
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
 
-__version__ = "2.0.0.post1"
+__version__ = "2.0.1"
 
 setup(version=__version__)
