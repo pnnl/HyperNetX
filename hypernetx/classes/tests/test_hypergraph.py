99067faa3224b05dfa59cd3fb827eb45229bb551
Brenda Praggastis <39808911+brendapraggastis@users.noreply.github.com>
2023-07-25 10:09:50 -0700

    updated hotfix in response to comments

--- hypernetx/classes/tests/test_hypergraph.py
+++ hypernetx/classes/tests/test_hypergraph.py
@@ -88,17 +88,20 @@ def test_remove_edges(sbs):
     H = H.remove_edges("O")
     assert H.shape == (6, 4)
 
-def test_remove_edges(triloop2):
+def test_remove(triloop2):
     H = triloop2.hypergraph
     k = "ACD2"
+    assert H.shape == (5,4)
     newH = H.remove(k)
     assert newH.shape == (5,3)
-    newH = H.remove_nodes('E')
+    newH = H.remove('E',level=1)
     assert newH.shape == (4,4)
-    newH = H.remove_edges('ACD')
+    newH = H.remove('ACD',level=0)
     assert newH.shape == (5,3)
     newH = H.remove(['ACD','E'])
     assert newH.shape == (4,3)
+    with pytest.raises(TypeError):
+        H.remove({'ACD':'edge'})
 
 
 def test_remove_nodes():
