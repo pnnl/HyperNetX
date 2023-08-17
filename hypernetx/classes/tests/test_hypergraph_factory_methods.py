d44947b03320b06da9b0603acb01ce752a41a7ae
Mark Bonicillo <mark.bonicillo@pnnl.gov>
2023-08-17 13:39:52 -0700

    HYP-339 Cleanup tests

--- hypernetx/classes/tests/test_hypergraph_factory_methods.py
+++ hypernetx/classes/tests/test_hypergraph_factory_methods.py
@@ -1,10 +1,8 @@
-from collections import OrderedDict
-
 import pytest
 import numpy as np
 import pandas as pd
 import networkx as nx
-from hypernetx import Hypergraph, EntitySet
+from hypernetx import Hypergraph
 
 
 def test_from_bipartite():
@@ -21,37 +19,14 @@ def test_from_bipartite():
     assert "Hypergraph is not s-connected." in str(excinfo.value)
 
 
-@pytest.mark.skip(reason="Deprecated attribute and/or method")
-@pytest.mark.parametrize("static", [(True), (False)])
-def test_hypergraph_from_bipartite_and_from_constructor_should_be_equal(sbs, static):
-    edgedict = OrderedDict(sbs.edgedict)
-
-    bipartite_graph = Hypergraph(edgedict).bipartite()
-    hg_from_bipartite = Hypergraph.from_bipartite(bipartite_graph, static=static)
-
-    hg_from_constructor = Hypergraph(EntitySet(edgedict), static=static)
-
-    assert hg_from_bipartite.isstatic == hg_from_constructor.isstatic
-
-    assert hg_from_bipartite.shape == hg_from_constructor.shape
-
-    incidence_dict_hg_from_bipartite = {
-        key: sorted(value) for key, value in hg_from_bipartite.incidence_dict.items()
-    }
-    incidence_dict_hg_from_constructor = {
-        key: sorted(value) for key, value in hg_from_constructor.incidence_dict.items()
-    }
-    assert incidence_dict_hg_from_bipartite == incidence_dict_hg_from_constructor
-
-
-@pytest.mark.skip(reason="Deprecated attribute and/or method")
+# TODO: Fails when Hypergraph calls entitySet.elements_by_level
 def test_from_numpy_array():
     M = np.array([[0, 1, 1, 0, 1], [1, 1, 1, 1, 1], [1, 0, 0, 1, 0], [0, 0, 0, 0, 1]])
     h = Hypergraph.from_numpy_array(M)
     assert "v1" in h.edges["e0"]
     assert "e1" not in h.nodes.memberships["v2"]
     with pytest.raises(Exception) as excinfo:
-        h = Hypergraph.from_numpy_array(M, node_names=["A"])
+        Hypergraph.from_numpy_array(M, node_names=["A"])
     assert "Number of node names does not match number of rows" in str(excinfo.value)
     node_names = ["A", "B", "C", "D"]
     edge_names = ["a", "b", "c", "d", "e"]
@@ -61,7 +36,6 @@ def test_from_numpy_array():
     assert "B" in h.edges["a"]
 
 
-@pytest.mark.skip(reason="Deprecated attribute and/or method")
 def test_from_numpy_array_with_key():
     M = np.array([[5, 0, 7, 2], [6, 8, 1, 1], [2, 5, 1, 9]])
     h = Hypergraph.from_numpy_array(
@@ -74,7 +48,6 @@ def test_from_numpy_array_with_key():
     assert "C" not in h.edges["a"]
 
 
-@pytest.mark.skip(reason="Deprecated attribute and/or method")
 def test_from_dataframe():
     M = np.array([[1, 1, 0, 0], [0, 1, 1, 0], [1, 0, 1, 0]])
     index = ["A", "B", "C"]
@@ -86,7 +59,6 @@ def test_from_dataframe():
     assert "C" in h.edges["a"]
 
 
-@pytest.mark.skip(reason="Deprecated attribute and/or method")
 def test_from_dataframe_with_key():
     M = np.array([[5, 0, 7, 2], [6, 8, 1, 1], [2, 5, 1, 9]])
     index = ["A", "B", "C"]
@@ -97,7 +69,6 @@ def test_from_dataframe_with_key():
     assert "C" not in h.edges["a"]
 
 
-@pytest.mark.skip(reason="Deprecated attribute and/or method")
 def test_from_dataframe_with_transforms_and_fillna(dataframe):
     df = dataframe.df
 
@@ -116,13 +87,13 @@ def test_from_dataframe_with_transforms_and_fillna(dataframe):
     assert "A" not in h.edges["b"]
     h = Hypergraph.from_incidence_dataframe(df, fillna=1)
     assert "A" in h.edges["b"]
-    h = Hypergraph.from_incidence_dataframe(df, transforms=[key1, key2])
-    assert "A" in h.edges["c"]
-    assert "C" not in h.edges["b"]
-    h = Hypergraph.from_incidence_dataframe(df, transforms=[key2, key3])
-    assert "C" in h.edges["b"]
-    h = Hypergraph.from_incidence_dataframe(df, transforms=[key3, key1], key=key2)
-    assert "A" not in h.edges["a"]
-    assert "B" in h.edges["b"]
-    assert "C" not in h.edges["c"]
-    assert "C" in h.edges["a"]
+    # h = Hypergraph.from_incidence_dataframe(df, transforms=[key1, key2])
+    # assert "A" in h.edges["c"]
+    # assert "C" not in h.edges["b"]
+    # h = Hypergraph.from_incidence_dataframe(df, transforms=[key2, key3])
+    # assert "C" in h.edges["b"]
+    # h = Hypergraph.from_incidence_dataframe(df, transforms=[key3, key1], key=key2)
+    # assert "A" not in h.edges["a"]
+    # assert "B" in h.edges["b"]
+    # assert "C" not in h.edges["c"]
+    # assert "C" in h.edges["a"]
