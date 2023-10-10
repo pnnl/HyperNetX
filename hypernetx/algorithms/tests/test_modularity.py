0f4b93f83312a94b5d1f4c6e28ebe3525b805d0b
nicol <nickibrt8@gmail.com>
2023-10-10 11:46:41 +0200

    added conductance test

--- hypernetx/algorithms/tests/test_modularity.py
+++ hypernetx/algorithms/tests/test_modularity.py
@@ -1,4 +1,7 @@
 import warnings
+import pytest
+
+
 from hypernetx.algorithms.hypergraph_modularity import *
 
 warnings.simplefilter("ignore")
@@ -27,3 +30,21 @@ def test_clustering(modularityexample):
     HG = precompute_attributes(HG)
     assert {"A", "B", "C"} in kumar(HG)
     assert {"C", "A", "B"} in last_step(HG, A4)
+
+
+def test_conductance(modularityexample):
+    HG = modularityexample.HG
+    A1, A2, A3, A4 = modularityexample.partitions
+    HG = precompute_attributes(HG)
+    assert conductance(HG, A1[0]) == 1 / 2
+    assert conductance(HG, A1[1]) == 4 / 7
+    assert conductance(HG, A2[0]) == 7 / 4
+    assert conductance(HG, A2[1]) == 7 / 11
+    with pytest.raises(Exception):
+        conductance(HG, A3[0])
+    assert conductance(HG, A4[0]) == 11 / 4
+    assert conductance(HG, A4[1]) == 5 / 2
+    assert conductance(HG, A4[2]) == 5 / 2
+    assert conductance(HG, A4[3]) == 3
+    assert conductance(HG, A4[4]) == 3
+    assert conductance(HG, A4[5]) == 8 / 3
