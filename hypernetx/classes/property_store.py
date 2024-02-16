9a2e0f158082ca9597cc8bfbb7b01026d4b603d7
Mark Bonicillo <mark.bonicillo@pnnl.gov>
2024-02-16 01:03:21 -0800

    Implement DataFramePropertyStore; add tests

--- hypernetx/classes/property_store.py
+++ hypernetx/classes/property_store.py
@@ -1,14 +1,14 @@
-from dataclasses import dataclass
 from abc import ABC, abstractmethod
-from typing import Union, Any
+from typing import Any, Optional
 
 import pandas as pd
 
+UID = "uid"
+WEIGHT = "weight"
+PROPERTIES = "properties"
 
-@dataclass
+
 class PropertyStore(ABC):
-    data: Any = None
-
     @property
     @abstractmethod
     def properties(self) -> Any:
@@ -16,12 +16,13 @@ class PropertyStore(ABC):
         ...
 
     @abstractmethod
-    def get_properties(self, key) -> dict[Any, Any]:
+    def get_properties(self, uid) -> dict[Any, Any]:
         """Get all properties of an item
 
         Parameters
         ----------
-        key : str | int
+        uid : tuple[str | int] | tuple[str | int, str | int ]
+            edge name, node name, or edge-node pair
 
         Returns
         -------
@@ -32,7 +33,7 @@ class PropertyStore(ABC):
         Raises
         ------
         KeyError
-            if (`key`) is not in :attr:`properties`,
+            if (`uid`) is not in :attr:`properties`,
 
         See Also
         --------
@@ -41,13 +42,13 @@ class PropertyStore(ABC):
         ...
 
     @abstractmethod
-    def get_property(self, key, prop_name) -> Any:
+    def get_property(self, uid, prop_name) -> Any:
         """Get a property of an item
 
         Parameters
         ----------
-        key : str | int
-            name of an item
+        uid : tuple[str | int] | tuple[str | int, str | int ]
+            edge name, node name, or edge-node pair
         prop_name : str | int
             name of the property to get
 
@@ -62,7 +63,7 @@ class PropertyStore(ABC):
         Raises
         ------
         KeyError
-            if (`key`) is not in :attr:`properties`,
+            if (`uid`) is not in :attr:`properties`,
 
         See Also
         --------
@@ -71,13 +72,13 @@ class PropertyStore(ABC):
         ...
 
     @abstractmethod
-    def set_property(self, key, prop_name, prop_val) -> None:
-        """Set a property of an item
+    def set_property(self, uid, prop_name, prop_val) -> None:
+        """Set a property of an item in the 'properties' dictionary
 
         Parameters
         ----------
-        key : str | int
-            name of an item
+        uid : tuple[str | int] | tuple[str | int, str | int ]
+            edge name, node name, or edge-node pair
         prop_name : str | int
             name of the property to set
         prop_val : any
@@ -85,8 +86,9 @@ class PropertyStore(ABC):
 
         Raises
         ------
-        ValueError
-            If `key` is not in :attr:`properties`
+        KeyError
+            If (`uid`) is not in :attr:`properties`
+
 
         See Also
         --------
@@ -112,12 +114,15 @@ class PropertyStore(ABC):
         """
         ...
 
-    def __getitem__(self, key: Union[str, int]) -> dict:
-        """Returns the common attributes (e.g. weight) and properties of a key in the underlying data table
+    def __getitem__(self, uid) -> dict:
+        """Returns the common attributes (e.g. weight) and properties of an item in the underlying data table
+
+        Returns same result as get_property
 
         Parameters
         ----------
-        key : str | int
+        uid : tuple[str | int] | tuple[str | int, str | int ]
+            edge name, node name, or edge-node pair
 
         Returns
         -------
@@ -125,12 +130,13 @@ class PropertyStore(ABC):
         """
         ...
 
-    def __contains__(self, key: Union[str, int]) -> bool:
-        """Returns true if key is in the underlying data table; false otherwise
+    def __contains__(self, uid) -> bool:
+        """Returns true if uid is in the underlying data table; false otherwise
 
         Parameters
         ----------
-        key : str | int
+        uid : tuple[str | int] | tuple[str | int, str | int ]
+            edge name, node name, or edge-node pair
 
         Returns
         -------
@@ -138,86 +144,98 @@ class PropertyStore(ABC):
         """
         ...
 
-    def __getattr__(self, key: Union[str, int]) -> dict:
-        """Returns the properties of a key in the underlying data table
 
-        Parameters
-        ----------
-        key : str | int
-
-        Returns
-        -------
-        dict
-
-        """
-        ...
-
-    def __setattr__(self, key: Union[str, int], value: dict) -> None:
-        """Sets the properties of a key in the underlying data table
-
-        Parameters
-        ----------
-        key : str | int
-        value : dict
-
-        Returns
-        -------
-        None
-        """
-        ...
-
-
-# TODO: Implement abstract methods and dunder methods using EntitySet code snippets
 class DataFramePropertyStore(PropertyStore):
+    """
+    data will be a dataframe of the following shape
+
+    uid | weight | properties | ...
+    (edge1) | 1.0 | {} | somepropname | somepropname2| ...
+
+    uid | weight | properties | ...
+    (node1) | 1.0 | {} | somepropname | somepropname2| ...
+
+    uid | weight | properties | ...
+    (edge1, node1) | 1.0 | {} | somepropname | somepropname2| ...
+
+
+     uid | weight | properties | ...
+    (edge1, node1) | 1.0 | {} | somepropname | somepropname2| ...
+    (edge1, node2) | 1.0 | {} | somepropname | somepropname2| ...
+
+    """
+
+    def __init__(self, data: pd.DataFrame):
+        self._data: pd.DataFrame = data
+
     @property
     def properties(self) -> pd.DataFrame:
         """Properties assigned to items in the underlying data table
 
         Returns
         -------
-        pandas.DataFrame a dataframe with the following columns: level/(edge|node), uid, weight, properties
+        pandas.DataFrame a dataframe with the following columns: uid, weight, properties, ...
         """
-        return self.data
+        return self._data
 
-    def get_properties(self, key) -> dict[Any, Any]:
-        pass
+    def get_properties(self, uid) -> dict[Any, Any]:
+        row_idx = self._uid_index(uid)
+        props = self._data.loc[row_idx].to_dict()
+        props.pop(UID)
+        return props
 
-    def get_property(self, key, prop_name) -> Any:
-        pass
+    def get_property(self, uid, prop_name) -> Any:
+        row_idx = self._uid_index(uid)
+        props = self._data.loc[row_idx].to_dict()
 
-    def set_property(self, key, prop_name, prop_val) -> None:
-        pass
+        if prop_name in props.get(PROPERTIES):
+            return props.get(PROPERTIES).get(prop_name)
+        if prop_name not in props:
+            return None
+        return props.pop(prop_name)
+
+    def set_property(self, uid, prop_name, prop_val) -> None:
+        row_idx = self._uid_index(uid)
+
+        if prop_name in self._data.columns and prop_name != PROPERTIES:
+            self._data.loc[row_idx, prop_name] = prop_val
+        else:
+            self._data.loc[row_idx, PROPERTIES].update({prop_name: prop_val})
+
+    def _uid_index(self, uid) -> int:
+        if uid not in self:
+            raise KeyError(f"uid, ({','.join(uid)}), not found in PropertyStore")
+        row_idx = self._data.index[self._data[UID] == uid].tolist()
+        return row_idx[0]
 
     def __iter__(self) -> iter:
-        return self.data.itertuples(name=self.level)
+        return self._data.itertuples(name="PropertyStore", index=False)
 
     def __len__(self) -> int:
-        return len(self.data.index)
+        return len(self._data.index)
 
-    def __getitem__(self, key: Union[str, int]) -> dict:
-        return self.data.loc[key].to_dict()
+    def __getitem__(self, uid) -> dict:
+        row_idx = self._uid_index(uid)
+        props = self._data.loc[row_idx].to_dict()
+        props.pop(UID)
+        return props
 
-    def __contains__(self, key: Union[str, int]) -> bool:
-        return key in self.data.index
-
-    def __getattr__(self, key: Union[str, int]) -> dict:
-        return self.data.loc[key, "properties"].to_dict()
-
-    def __setattr__(self, key: Union[str, int], value: dict) -> None:
-        self.data.loc[key, "properties"] = value
+    def __contains__(self, uid) -> bool:
+        idx = self._data.index[self._data[UID] == uid]
+        return not idx.empty
 
 
-# This class is shown to demonstrate the flexibility of making PropertyStore an abstract class
-class DictPropertyStore(PropertyStore):
-    @property
-    def properties(self) -> dict:
-        pass
-
-    def get_properties(self, key) -> dict[Any, Any]:
-        pass
-
-    def get_property(self, key, prop_name) -> Any:
-        pass
-
-    def set_property(self, key, prop_name, prop_val) -> None:
-        pass
+# TODO: Implement the dictionary version of PropertyStore
+# class DictPropertyStore(PropertyStore):
+#     @property
+#     def properties(self) -> dict:
+#         pass
+#
+#     def get_properties(self, uid) -> dict[Any, Any]:
+#         pass
+#
+#     def get_property(self, uid, prop_name) -> Any:
+#         pass
+#
+#     def set_property(self, uid, prop_name, prop_val) -> None:
+#         pass
--- hypernetx/classes/tests/test_property_store.py
+++ hypernetx/classes/tests/test_property_store.py
@@ -1,4 +1,239 @@
-from hypernetx.classes.property_store import PropertyStore
+import pytest
+import pandas as pd
+from hypernetx.classes.property_store import PropertyStore, DataFramePropertyStore
 
 
-def test_property_store(): ...
+@pytest.fixture
+def edges() -> list[tuple[str]]:
+    return [("P",), ("R",), ("S",), ("L",), ("M",), ("N",)]
+
+
+@pytest.fixture
+def df_edges(edges) -> pd.DataFrame:
+    uid = edges
+    data = {
+        "uid": uid,
+        "weight": [1.0 for _ in range(len(uid))],
+        "properties": [{} for _ in range(len(uid))],
+    }
+    return pd.DataFrame.from_dict(data)
+
+
+@pytest.fixture
+def dfps_edges(df_edges) -> PropertyStore:
+    return DataFramePropertyStore(df_edges)
+
+
+@pytest.fixture
+def incident_pairs() -> list[tuple[str, str]]:
+    return [
+        ("P", "K"),
+        ("P", "A"),
+        ("P", "C"),
+        ("R", "A"),
+        ("R", "E"),
+        ("S", "K"),
+        ("S", "A"),
+        ("S", "V"),
+        ("S", "T2"),
+        ("L", "E"),
+        ("L", "C"),
+        ("M", "T2"),
+        ("M", "T1"),
+        ("N", "K"),
+        ("N", "T2"),
+    ]
+
+
+@pytest.fixture
+def df_incident_pairs(incident_pairs) -> pd.DataFrame:
+    uid = incident_pairs
+    data = {
+        "uid": uid,
+        "weight": [1.0 for _ in range(len(uid))],
+        "properties": [{} for _ in range(len(uid))],
+        "strength": [42 for _ in range(len(uid))],
+        "hair_color": ["red" for _ in range(len(uid))],
+    }
+    return pd.DataFrame.from_dict(data)
+
+
+@pytest.fixture
+def dfps_incident_pairs(df_incident_pairs) -> PropertyStore:
+    return DataFramePropertyStore(df_incident_pairs)
+
+
+def test_properties(dfps_edges, edges):
+    props = dfps_edges.properties
+    assert isinstance(props, pd.DataFrame)
+    # check that all edges are in 'properties'
+    assert all(uid in props.get("uid").tolist() for uid in edges)
+    assert all(weight == 1.0 for weight in props.get("weight").tolist())
+    assert all(prop == dict() for prop in props.get("properties").tolist())
+
+
+@pytest.mark.parametrize(
+    "fixture, uid, expected",
+    [
+        ("dfps_edges", ("P",), {"weight": 1.0, "properties": dict()}),
+        (
+            "dfps_incident_pairs",
+            ("S", "A"),
+            {"weight": 1.0, "properties": dict(), "hair_color": "red", "strength": 42},
+        ),
+    ],
+)
+def test_get_properties(fixture, uid, expected, request):
+    ps = request.getfixturevalue(fixture)
+    props = ps.get_properties(uid)
+    assert props == expected
+
+
+@pytest.mark.parametrize(
+    "fixture, uid",
+    [("dfps_edges", ("NEMO",)), ("dfps_incident_pairs", ("NE", "MO"))],
+)
+def test_get_properties_raises_key_error(fixture, uid, request):
+    with pytest.raises(KeyError) as exc_info:
+        ps = request.getfixturevalue(fixture)
+        ps.get_properties(uid)
+
+    assert (
+        str(exc_info.value)
+        == f"""'uid, ({",".join(uid)}), not found in PropertyStore'"""
+    )
+
+
+@pytest.mark.parametrize(
+    "fixture, uid, prop_name, expected",
+    [
+        ("dfps_edges", ("P",), "weight", 1.0),
+        ("dfps_edges", ("P",), "properties", dict()),
+        ("dfps_incident_pairs", ("S", "A"), "weight", 1.0),
+        ("dfps_incident_pairs", ("S", "A"), "properties", dict()),
+        ("dfps_incident_pairs", ("S", "A"), "strength", 42),
+        ("dfps_incident_pairs", ("S", "A"), "hair_color", "red"),
+        ("dfps_edges", ("P",), "NOT A PROPERTY", None),
+        ("dfps_incident_pairs", ("S", "A"), "not a property", None),
+    ],
+)
+def test_get_property(fixture, uid, prop_name, expected, request):
+    ps = request.getfixturevalue(fixture)
+    props = ps.get_property(uid, prop_name)
+    assert props == expected
+
+
+@pytest.mark.parametrize(
+    "fixture, uid, prop_name",
+    [
+        ("dfps_edges", ("NEMO",), "weight"),
+        ("dfps_incident_pairs", ("NE", "MO"), "weight"),
+    ],
+)
+def test_get_property_raises_key_error(fixture, uid, prop_name, request):
+    with pytest.raises(KeyError) as exc_info:
+        ps = request.getfixturevalue(fixture)
+        ps.get_property(uid, prop_name)
+
+    assert (
+        str(exc_info.value)
+        == f"""'uid, ({",".join(uid)}), not found in PropertyStore'"""
+    )
+
+
+@pytest.mark.parametrize(
+    "fixture, uid, prop_name, prop_val, current_props",
+    [
+        ("dfps_edges", ("P",), "weight", 123.0, 1.0),
+        ("dfps_edges", ("P",), "cost", 42.42, None),
+        ("dfps_incident_pairs", ("S", "A"), "weight", 123.0, 1.0),
+        ("dfps_incident_pairs", ("S", "A"), "cost", 42.42, None),
+        ("dfps_incident_pairs", ("S", "A"), "strength", 999, 42),
+        ("dfps_incident_pairs", ("S", "A"), "hair_color", "blue", "red"),
+    ],
+)
+def test_set_property(fixture, uid, prop_name, prop_val, current_props, request):
+    ps = request.getfixturevalue(fixture)
+
+    props = ps.get_property(uid, prop_name)
+    assert props == current_props
+
+    ps.set_property(uid, prop_name, prop_val)
+
+    props = ps.get_property(uid, prop_name)
+
+    assert props == prop_val
+
+
+@pytest.mark.parametrize(
+    "fixture, uid, prop_name, prop_val",
+    [
+        ("dfps_edges", ("NEMO",), "cost", 42.42),
+        ("dfps_incident_pairs", ("NE", "MO"), "hair_color", "red"),
+    ],
+)
+def test_set_property_raises_key_error(fixture, uid, prop_name, prop_val, request):
+    with pytest.raises(KeyError) as exc_info:
+        ps = request.getfixturevalue(fixture)
+        ps.set_property(uid, prop_name, prop_val)
+
+    assert (
+        str(exc_info.value)
+        == f"""'uid, ({",".join(uid)}), not found in PropertyStore'"""
+    )
+
+
+@pytest.mark.parametrize(
+    "fixture, uids",
+    [
+        ("dfps_edges", "edges"),
+        ("dfps_incident_pairs", "incident_pairs"),
+    ],
+)
+def test_iter(fixture, uids, request):
+    ps = request.getfixturevalue(fixture)
+    entities = request.getfixturevalue(uids)
+    assert all([i.uid in entities for i in ps])
+
+
+@pytest.mark.parametrize(
+    "fixture, expected",
+    [
+        ("dfps_edges", 6),
+        ("dfps_incident_pairs", 15),
+    ],
+)
+def test_len(fixture, expected, request):
+    ps = request.getfixturevalue(fixture)
+    assert len(ps) == expected
+
+
+@pytest.mark.parametrize(
+    "fixture, uid, expected",
+    [
+        ("dfps_edges", ("P",), {"weight": 1.0, "properties": dict()}),
+        (
+            "dfps_incident_pairs",
+            ("S", "A"),
+            {"weight": 1.0, "properties": dict(), "hair_color": "red", "strength": 42},
+        ),
+    ],
+)
+def test_getitem(fixture, uid, expected, request):
+    ps = request.getfixturevalue(fixture)
+    props = ps[uid]
+    assert props == expected
+
+
+@pytest.mark.parametrize(
+    "fixture, uid, expected",
+    [
+        ("dfps_edges", ("P",), True),
+        ("dfps_edges", ("NEMO",), False),
+        ("dfps_incident_pairs", ("S", "A"), True),
+        ("dfps_incident_pairs", ("NE", "MO"), False),
+    ],
+)
+def test_contains(fixture, uid, expected, request):
+    ps = request.getfixturevalue(fixture)
+    assert (uid in ps) == expected
