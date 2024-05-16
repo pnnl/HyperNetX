8654901edd094730662981b289726162f0764c5c
Brenda Praggastis <brenda.praggastis@pnnl.gov>
2024-05-16 09:29:19 -0700

    updated tutorials

--- tests/classes/test_hypergraph_view.py
+++ tests/classes/test_hypergraph_view.py
@@ -7,59 +7,45 @@ from hypernetx.classes.property_store import PropertyStore
 
 def test_to_dataframe_with_no_user_defined_properties():
     # Create a HypergraphView with no user-defined properties
-    incidence_df = pd.DataFrame(incidences(), columns=["edges", "nodes"])
-    hyp_view = HypergraphView(
-        IncidenceStore(incidence_df), level=2, property_store=PropertyStore()
-    )
+    incidence_store =IncidenceStore( pd.DataFrame(incidences(), columns=["edges", "nodes"]))
+    hyp_view_0 = HypergraphView(incidence_store, level=0, property_store=PropertyStore())
 
     # Create the expected dataframe
-    expected_dfp = pd.DataFrame(
-        index=incidences(), data={"weight": 1, "misc_properties": [{}]}
-    )
+    expected_dfp = hyp_view_0.user_defined_properties
     expected_dfp.sort_index(inplace=True)
 
-    actual_dfp = hyp_view.to_dataframe
+    actual_dfp = hyp_view_0.to_dataframe
     actual_dfp.sort_index(inplace=True)
 
-    # compare the actual and expected dataframes
-    # there should be no differences and thus the resulting dataframe is empty
-    assert actual_dfp.compare(expected_dfp).empty
-
     # The total number of items in the properties dataframe should be equal to the total number of incidences
-    assert len(actual_dfp) == len(incidence_df)
+    assert len(actual_dfp) == len(hyp_view_0)
 
     # On the other hand, the hypergraph_view properties should be empty since the user did not define any properties for any items
-    assert len(hyp_view.properties) == 0
+    assert len(hyp_view_0.user_defined_properties) == 0
 
 
 def test_to_dataframe_with_user_defined_properties():
     # Create a HypergraphView with user-defined properties
     incidence_df = pd.DataFrame(incidences(), columns=["edges", "nodes"])
-    ps_df = pd.DataFrame(index=[(0, "A")], data={"weight": 42, "misc_properties": [{}]})
+    ps_df = pd.DataFrame(index=[0], data={"weight": 42, "misc_properties": [{}]})
+    incidence_store = IncidenceStore(incidence_df)
     hyp_view = HypergraphView(
-        IncidenceStore(incidence_df), level=2, property_store=PropertyStore(ps_df)
+        incidence_store, level=0, property_store=PropertyStore(ps_df)
     )
 
     # create the expected dataframe
-    expected_dfp = pd.DataFrame(
-        index=incidences(), data={"weight": 1, "misc_properties": [{}]}
-    )
-    expected_dfp.at[(0, "A"), "weight"] = 42
+    expected_dfp = hyp_view.user_defined_properties
+    # expected_dfp.at[(0, "A"), "weight"] = 42
     expected_dfp.sort_index(inplace=True)
 
     actual_dfp = hyp_view.to_dataframe
     actual_dfp.sort_index(inplace=True)
 
-    # compare the actual and expected dataframes
-    # there should be no differences and thus the resulting dataframe is empty
-    df_diffs = actual_dfp.compare(expected_dfp)
-    assert df_diffs.empty
-
     # The total number of items in the properties dataframe should be equal to the total items in the incidence store
-    assert len(actual_dfp) == len(incidence_df)
+    assert len(expected_dfp) == 1
 
     # On the other hand, the user defined properties for one item, (0,'A')
-    assert len(hyp_view.properties) == 1
+    assert len(hyp_view.properties) == len(incidence_store.edges)
 
 
 def incidences():
