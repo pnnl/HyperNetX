44b897fd3fc703000f575f996cf850c26a54409c
Shira <shirarot047@gmail.com>
2024-06-02 12:34:07 +0300

    update test & algo

--- hypernetx/algorithms/matching_algorithms.py
+++ hypernetx/algorithms/matching_algorithms.py
@@ -1,46 +1,45 @@
-
 """
 An implementation of the algorithms in:
 "Distributed Algorithms for Matching in Hypergraphs", by Oussama Hanguir and Clifford Stein (2020), https://arxiv.org/abs/2009.09605v1
-Programmer: shira rot , niv
+Programmer: Shira Rot, Niv
 Date: 22.5.2024
 """
 
 from hypernetx.classes.hypergraph import Hypergraph
 
-def greedy_d_approximation(hypergraph: Hypergraph, int: Hypergraph.degree()) -> list:
+def greedy_d_approximation(hypergraph: Hypergraph) -> list:
     """
     Algorithm 1: Greedy d-Approximation for Hypergraph Matching
     Finds a greedy d-approximation for hypergraph matching.
 
     Parameters:
     hypergraph (Hypergraph): A Hypergraph object.
-    d (int): The size of each hyperedge, assuming the hypergraph is d-uniform.
 
     Returns:
     list: the edges of the graph for the approximate matching.
 
     Examples:
     >>> hypergraph = Hypergraph({0: (1, 2, 3), 1: (4, 5, 6)})
-    >>> greedy_d_approximation(hypergraph, 3)
+    >>> greedy_d_approximation(hypergraph)
     [(1, 2, 3), (4, 5, 6)]
 
     >>> hypergraph = Hypergraph({0: (1, 2, 3), 1: (4, 5, 6), 2: (7, 8, 9), 3: (1, 4, 7), 4: (2, 5, 8), 5: (3, 6, 9)})
-    >>> greedy_d_approximation(hypergraph, 3)
+    >>> greedy_d_approximation(hypergraph)
     [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
 
 
     >>> hypergraph = Hypergraph({0: (1, 2, 3), 1: (2, 3, 4), 2: (3, 4, 5), 3: (5, 6, 7), 4: (6, 7, 8), 5: (7, 8, 9)})
-    >>> greedy_d_approximation(hypergraph, 3)
+    >>> greedy_d_approximation(hypergraph)
     [(1, 2, 3), (5, 6, 7)]
 
 
     >>> hypergraph = Hypergraph({0: (1, 2, 3, 4), 1: (5, 6, 7, 8), 2: (9, 10, 11, 12), 3: (13, 14, 15, 1), 4: (2, 6, 10, 14), 5: (3, 7, 11, 15), 6: (4, 8, 12, 1), 7: (5, 9, 13, 2), 8: (6, 10, 14, 3), 9: (7, 11, 15, 4)})
-    >>> greedy_d_approximation(hypergraph, 4)
+    >>> greedy_d_approximation(hypergraph)
     [(1, 2, 3, 4), (5, 6, 7, 8), (9, 10, 11, 12), (13, 14, 15, 1)]
 
     """
-    return []  # Empty implementation
+    return []
+
 
 def hedcs_based_approximation(hypergraph: Hypergraph, d: int, s: int) -> list:
     """
@@ -72,7 +71,7 @@ def hedcs_based_approximation(hypergraph: Hypergraph, d: int, s: int) -> list:
     >>> hedcs_based_approximation(hypergraph, 4, 3)
     [(1, 2, 3, 4), (5, 6, 7, 8), (9, 10, 11, 12)]
     """
-    return []  # Empty implementation
+    return []
 
 def iterated_sampling(hypergraph: Hypergraph, d: int ,s: int) -> list:
     """
@@ -87,7 +86,6 @@ def iterated_sampling(hypergraph: Hypergraph, d: int ,s: int) -> list:
     Returns:
     list: The edges of the graph for the approximate matching.
 
-    Examples:
     Examples:
     >>> hypergraph = Hypergraph({0: (1, 2, 3), 1: (4, 5, 6)})
     >>> iterated_sampling(hypergraph, 3, 10)
@@ -105,7 +103,7 @@ def iterated_sampling(hypergraph: Hypergraph, d: int ,s: int) -> list:
     >>> iterated_sampling(hypergraph, 4, 10)
     [(1, 2, 3, 4), (5, 6, 7, 8)] or [(9, 10, 11, 12), (13, 14, 15, 1)]
     """
-    return None  # Empty implementation
+    return []
 
 if __name__ == '__main__':
     import doctest
