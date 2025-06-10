6b9be448ee5eef2d2427df0ed1da99d561f47273
Dustin Arendt <dustin.arendt@pnnl.gov>
2025-06-10 10:36:20 -0700

    Added data generators for storyline test datasets.

---
+++ hypernetx/drawing/storyline_generators.py
@@ -0,0 +1,204 @@
+import hypernetx as hnx
+
+from collections import defaultdict
+from functools import lru_cache
+import re
+
+import urllib
+
+import networkx as nx
+
+import numpy as np
+
+def from_bipartite(G):
+    incidences = defaultdict(list)
+    
+    for v, e in G.edges():
+        incidences[e].append(v)
+
+    return hnx.Hypergraph(incidences)
+
+def create_lesmis_small():
+    scenes = {
+        0: ('FN', 'TH'),
+        1: ('TH', 'JV'),
+        2: ('BM', 'FN', 'JA'),
+        3: ('JV', 'JU', 'CH', 'BM'),
+        4: ('JU', 'CH', 'BR', 'CN', 'CC', 'JV', 'BM'),
+        5: ('TH', 'GP'),
+        6: ('GP', 'MP'),
+        7: ('MA', 'GP')
+    }
+
+    return hnx.Hypergraph(scenes)
+
+def create_davis_southern_women():
+    return from_bipartite(nx.davis_southern_women_graph())
+
+def create_star_wars():
+    VADER = 'Vader'
+    LEIA = 'Leia'
+    R2 = 'R2-D2'
+    C3PO = 'C-3PO'
+    OBIWAN = 'Obi-Wan'
+    LUKE = 'Luke'
+    HAN = 'Han'
+    CHEWIE = 'Chewie'
+    JABBA = 'Jabba'
+    
+    events = [
+        {VADER},
+        {LEIA, R2},
+        {C3PO},
+        {OBIWAN},
+        {LUKE},
+        {HAN, CHEWIE},
+        {JABBA},
+        {VADER, LEIA},
+        {R2, C3PO},
+        {R2, C3PO, LUKE},
+        {OBIWAN, R2, C3PO, LUKE},
+        {OBIWAN, R2, C3PO, LUKE, HAN, CHEWIE, JABBA},
+        {LEIA, LUKE, HAN, CHEWIE}, # Leia rescued
+        {R2, C3PO},
+        {OBIWAN, VADER}, # Duel
+        {VADER, LUKE, R2}, # Death star
+        {C3PO, LEIA},
+        {VADER, LUKE, R2, HAN, CHEWIE},
+        {VADER},
+        {LUKE, R2, HAN, CHEWIE, LEIA, C3PO},
+        {LUKE},
+        {JABBA}
+    ]
+
+    def rgb(*args):
+        return np.array(args)/255.
+
+    H = hnx.Hypergraph(dict(enumerate(events)))
+
+    node_colors = defaultdict(
+        lambda: rgb(158, 158, 158),
+        **{
+            VADER: rgb(0, 0, 0),
+            C3PO: rgb(208, 196, 0),
+            R2: rgb(46, 124, 230),
+            CHEWIE: rgb(167, 100, 40),
+            JABBA: rgb(130, 189, 119)
+        }
+    )
+    
+    labels = [
+        ({LEIA, LUKE, HAN, CHEWIE}, 'Leia\nrescued'),
+        ({OBIWAN, VADER}, 'Duel'),
+        ({VADER, LUKE, R2}, 'Death\nStar')
+    ]
+    
+    edge_labels = defaultdict(lambda: '')
+    for k, v in labels:
+        edge_labels[events.index(k)] = v
+
+    kwargs = dict(
+        nodes_kwargs={
+            'edgecolor': node_colors
+        },
+        edges_kwargs={
+            'edgecolor': None,
+            'facecolor': rgb(211, 211, 211)
+        },
+        edge_labels=edge_labels
+    )
+
+    return H, kwargs
+
+@lru_cache()
+def load_file(path_or_url, encoding='utf-8'):
+    print('Reading', path_or_url)
+    
+    with urllib.request.urlopen(path_or_url) as fp:
+        return fp.read().decode(encoding)
+
+def parse_play(
+    path_or_url,
+    encoding='utf-8',
+    start=None, end=None,
+    start_str=None, end_str=None,
+    ignore=set(),
+    replace=dict(),
+    return_text=False,
+    split_act='ACT [IXV]+',
+    split_scene='SCENE [IXV]+',
+):
+    
+    txt = load_file(path_or_url, encoding)\
+        .replace('\r', '')
+
+    if start_str:
+        start = txt.index(start_str)
+
+    if end_str:
+        end = txt.index(end_str)
+
+    txt = txt[start:end]
+    
+    edges = {}
+
+    for i, act in enumerate(re.split(split_act, txt)[1:]):
+        for j, scene in enumerate(re.split(split_scene, act)[1:]):
+            nodes = set(
+                map(
+                    str.strip,
+                    re.findall('([A-Z ]+).\n', scene)
+                )
+            ).difference(ignore)
+            
+            edges[f'{i + 1}.{j + 1}'] = set([
+                replace.get(v, v)
+                for v in nodes
+            ])
+
+    H = hnx.Hypergraph(edges)
+    
+    if return_text:
+        return H, txt
+        
+    return H
+
+def create_macbeth(path=None):
+    return parse_play(
+        path or 'https://www.gutenberg.org/cache/epub/1533/pg1533.txt',
+        start_str='ACT I\n\nSCENE I.',
+        end_str='*** END OF THE PROJECT GUTENBERG',
+        ignore = {
+            '',
+            'I',
+            'ALL',
+            'BOTH MURDERERS'
+        },
+        replace={
+            'MURDERER': 'FIRST MURDERER',
+            'LORDS': 'LORD'
+        }
+    )
+
+def create_hamlet(path=None):
+    return parse_play(
+        path or 'https://www.gutenberg.org/cache/epub/1524/pg1524.txt',
+        start_str='ACT I\n\nSCENE I.',
+        end_str='*** END OF THE PROJECT GUTENBERG',
+        ignore = {
+            '',
+            'I',
+            'T',
+            'ALL',
+            'BOTH'
+        },
+        replace={
+            'BARNARD': 'BARNARDO',
+            'LORD': 'LORDS',
+            'FIRST CLOWN': 'CLOWNS',
+            'SECOND CLOWN': 'CLOWNS',
+            'PLAYER KING': 'PLAYER KING & QUEEN',
+            'PLAYER QUEEN': 'PLAYER KING & QUEEN',
+        }
+    )
+    
