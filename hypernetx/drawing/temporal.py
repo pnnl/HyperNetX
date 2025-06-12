2a2d9c5a9a36749a8b90d1e1d9036eeb85834ab5
Dustin Arendt <dustin.arendt@pnnl.gov>
2025-06-12 16:36:14 -0700

    Utilities for finding and measuring temporal paths.

---
+++ hypernetx/drawing/temporal.py
@@ -0,0 +1,101 @@
+import matplotlib.pyplot as plt
+
+import hypernetx as hnx
+from hypernetx.drawing import draw_storyline as ds
+
+import networkx as nx
+
+from collections import defaultdict
+
+def create_temporal_incidence_graph(H, edge_pos=None, edge_order=None, weight_by_time=True):
+    assert (edge_pos is None) ^ (edge_order is None),\
+        'Exactly one of edge_pos and edge_order must be specified'
+    
+    if edge_pos is None:
+        edge_pos = {
+            v: i
+            for i, v in enumerate(edge_order)
+        }
+
+    G = nx.DiGraph()
+    
+    for v in H.nodes():
+        edges = sorted(H.nodes[v], key=edge_pos.get)
+        for e in edges:
+            w = 0 if weight_by_time else 1
+            
+            G.add_edge((e, v), e, weight=w)
+            G.add_edge(e, (e, v), weight=0)
+    
+        # temporal/directed edges connecting incidences
+        for e1, e2 in zip(edges[:-1], edges[1:]):
+            w = edge_pos[e2] - edge_pos[e1] if weight_by_time else 0
+            G.add_edge((e1, v), (e2, v), weight=w)
+    
+    return G
+
+def multi_source_target_dijkstra(G, sources, targets, **kwargs):
+    return min(
+        [    
+            nx.multi_source_dijkstra(
+                G,
+                sources=sources,
+                target=t,
+                **kwargs
+            )
+            for t in targets
+        ],
+        key=lambda x: x[0]
+    )
+
+def find_incidences(H, x):
+    if type(x) is tuple:
+        return [x]
+    elif x in H.edges:
+        return [(x, v) for v in H.edges[x]]
+    elif x in H.nodes:
+        return [(e, x) for e in H.nodes[x]]
+    return []
+
+def temporal_shortest_path(H, source, target, **kwargs):
+    G = create_temporal_incidence_graph(H, **kwargs)
+
+    return multi_source_target_dijkstra(
+        G,
+        sources=find_incidences(H, source),
+        targets=find_incidences(H, target),
+        weight='weight'
+    )
+
+def draw_path(H, edge_order, path, **kwargs):
+    endpoints = {path[0], path[-1]}
+    segments = set(zip(path[:-1], path[1:]))
+    
+    return ds.draw_storyline(
+        H,
+        edge_order=edge_order,
+        edges_kwargs=dict(
+            edgecolor='darkgray'
+        ),
+        incidences_kwargs=dict(
+            facecolor=defaultdict(
+                lambda: 'white', 
+                {
+                    i: 'red' if i in endpoints else 'black'
+                    for i in path
+                }
+            ),
+            # edgecolor='pink'
+        ),
+        segments_kwargs=dict(
+            linewidths=lambda seg: 3 if seg in segments else 1
+        ),
+        fill_edges=defaultdict(
+            lambda: False,
+            {
+                i: True
+                for i in path
+            }
+        ),
+        **kwargs
+    )
