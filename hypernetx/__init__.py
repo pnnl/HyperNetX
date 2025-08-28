59983c02ade968df0da5494a4cf2b05da8168c2c
Dustin Arendt <dustin.arendt@pnnl.gov>
2025-08-28 11:48:33 -0700

    For now, import all algorithms to the hypernetx namespace--we'll have to address how to deprecate this approach later.

--- hypernetx/__init__.py
+++ hypernetx/__init__.py
@@ -7,7 +7,7 @@ from hypernetx.hif import to_hif, from_hif
 from hypernetx.classes import *
 from hypernetx.reports import *
 from hypernetx.drawing import *
-# from hypernetx.algorithms import *
+from hypernetx.algorithms import *
 from hypernetx.utils import *
 from hypernetx.utils.toys import *
 
